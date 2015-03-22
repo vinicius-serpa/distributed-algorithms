36118839713f83998c8bebb3938493f0baec3aa5
vserpa <vinicius-serpa@live.com>
2015-03-22 19:40:39 -0300

    Exercise List 1

---
+++ study/src/list1/event/ErrorEvent.java
@@ -0,0 +1,21 @@
+package list1.event;
+
+import net.sf.appia.core.Event;
+
+/**
+ * Job Submit error event.
+ * 
+ * @author carlos.mestre
+ */
+public class ErrorEvent extends Event {
+	
+	int rqid;
+
+	public void setId(int rid) {
+		rqid = rid;
+	}
+
+	public int getId() {
+		return rqid;
+	}
+}
---
+++ study/src/list1/layer/JobHandlerLayer.java
@@ -0,0 +1,38 @@
+package list1.layer;
+
+import list1.event.ConfirmEvent;
+import list1.event.SubmitEvent;
+import list1.session.JobHandlerSession;
+import net.sf.appia.core.Layer;
+import net.sf.appia.core.Session;
+import net.sf.appia.core.events.channel.ChannelInit;
+
+/**
+ * Layer of the JobHandler protocol.
+ * 
+ * @author carlos.mestre
+ */
+public class JobHandlerLayer extends Layer {
+
+	public JobHandlerLayer() {
+
+		/* events that the protocol will create */
+		evProvide = new Class[1];
+		evProvide[0] = ConfirmEvent.class;
+
+		/*
+		 * events that the protocol requires to work This is a subset of the
+		 * accepted events.
+		 */
+		evRequire = new Class[0];
+
+		/* events that the protocol will accept */
+		evAccept = new Class[2];
+		evAccept[0] = SubmitEvent.class;
+		evAccept[1] = ChannelInit.class;
+	}
+
+	public Session createSession() {
+		return new JobHandlerSession(this);
+	}
+}
